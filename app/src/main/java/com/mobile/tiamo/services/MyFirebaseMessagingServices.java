1b10552257c0be00965e8b6171937682074c51ab
nguyenkien1402 <nguyenkien1402@gmail.com>
2019-09-16 15:40:12 +1000

    Add the firebase messaging service

---
+++ app/src/main/java/com/mobile/tiamo/services/MyFirebaseMessagingServices.java
@@ -0,0 +1,103 @@
+package com.mobile.tiamo.services;
+
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.mobile.tiamo.R;
+import com.mobile.tiamo.activities.NotificationActivity;
+import com.mobile.tiamo.utilities.Config;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+
+import java.util.Map;
+
+
+public class MyFirebaseMessagingServices extends FirebaseMessagingService {
+
+    private static final String TAG = "FirebaseMS";
+    private String message = "";
+
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        if(remoteMessage.getData() != null){
+            getDateForNotification(remoteMessage);
+        }
+    }
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+        Log.d(TAG,"Token: "+s);
+    }
+
+    private void sendNotification(){
+
+        Log.d(TAG,"Send notification");
+
+        Uri defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Intent intent = new Intent(getApplicationContext(), NotificationActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent , 0);
+        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID = "101";
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+
+            @SuppressLint("WrongConstant") NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "Notification", NotificationManager.IMPORTANCE_MAX);
+
+            notificationChannel.setDescription("G");
+            notificationChannel.enableLights(true);
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationChannel.enableVibration(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+                    .setSmallIcon(R.mipmap.ic_launcher_round)
+                    .setContentTitle(Config.title)
+                    .setAutoCancel(true)
+                    .setSound(defaultSound)
+                    .setContentText(Config.content)
+                    .setContentIntent(pendingIntent)
+                    .setWhen(System.currentTimeMillis())
+                    .setPriority(Notification.PRIORITY_MAX);
+
+            notificationManager.notify(1, notificationBuilder.build());
+    }
+
+    private void getDateForNotification(final RemoteMessage remoteMessage) {
+        try{
+            Map<String, String> data = remoteMessage.getData();
+            Config.id = Integer.parseInt(data.get("id"));
+            Config.title = data.get("title");
+            Config.content = data.get("content");
+            Config.imageUrl = data.get("imageUrl");
+            Config.gameUrl = data.get("gameUrl");
+            Config.patientId = data.get("patientId");
+            sendNotification();
+        }catch (Exception e){
+            Log.d(TAG,e.getMessage());
+        }
+
+    }
+}
---
+++ app/src/main/java/com/mobile/tiamo/utilities/Config.java
@@ -0,0 +1,12 @@
+package com.mobile.tiamo.utilities;
+
+public class Config {
+
+    public static int id = 0;
+    public static String content = "";
+    public static String title = "";
+    public static String imageUrl = "";
+    public static String gameUrl = "";
+    public static String patientId  = "";
+
+}
